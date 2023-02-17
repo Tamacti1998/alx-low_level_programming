201c063f7c8e4a5b9386f982dcf20ec39c89b2e1
Tamacti Jun <awalalhassan800@gmail.com>
2023-02-17 12:58:09 -0800

    Task 5 commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- * main - C program that display a line using the printf function
+ *main - print with proper grammar, but the outcome is a piece of art,
  *
- * Return: 0 when successful
+ * Return: 0 if exited properly, non zero if otherwise
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
-return (0);
-}
 
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
