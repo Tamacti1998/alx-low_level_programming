720d3d39f672b15be9b59bf5aaa862ea70f56c8c
Tamacti Jun <awalalhassan800@gmail.com>
2023-02-17 07:35:39 -0800

    Task 5 commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - C program that display a line using the printf function
+ *
+ * Return: 0 when successful
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
+
