d17a63d2ba358a630f82c6f21568353322365643
Tamacti Jun <awalalhassan800@gmail.com>
2023-02-17 07:24:45 -0800

    Task 4 commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- * main - this is the main function
- * 
- * Return: always 0
- */ 
-
+ * main - C program that display a line using the put function
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
