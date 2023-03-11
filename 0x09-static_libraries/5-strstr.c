8d2a69e1910d9b62a1e656611e601e08a8e8b5b9
Tamacti Jun <awalalhassan800@gmail.com>
2023-03-11 04:37:27 -0800

    Task 0 commit

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,29 +1,48 @@
 #include "main.h"
+
 /**
- * _strstr - Entry point
- * @haystack: input
- * @needle: input
- * Return: Always 0 (Success)
+ * *_strstr - function declaration
+ * @haystack: the string to be searched
+ * @needle: the substring
+ * Return: string or NULL
  */
+
+char *_strstr(char *haystack, char *needle);
+
+/**
+ * *_strstr - function definition
+ * @haystack: that string that will be searched
+ * @needle: the substring to search for
+ * Description: search for needle in haystack
+ * Return: string or NULL
+ */
+
 char *_strstr(char *haystack, char *needle)
 {
-	for (; *haystack != '\0'; haystack++)
+	int hay_index;
+	int needle_index;
+
+	if (needle[0] == '\0')
 	{
-		char *l = haystack;
-		char *p = needle;
-
-
-		while (*l == *p && *p != '\0')
-		{
-			l++;
-			p++;
-		}
-
-
-		if (*p == '\0')
-			return (haystack);
+		return (haystack);
 	}
 
-
-	return (0);
+	for (hay_index = 0; haystack[hay_index] != '\0'; hay_index++)
+	{
+		if (haystack[hay_index] == needle[0])
+		{
+			for (needle_index = 0; needle[needle_index] != '\0'; needle_index++)
+			{
+				if (haystack[hay_index + needle_index] != needle[needle_index])
+				{
+					break;
+				}
+			}
+			if (needle[needle_index] == '\0')
+			{
+				return (haystack + hay_index);
+			}
+		}
+	}
+	return ('\0');
 }
